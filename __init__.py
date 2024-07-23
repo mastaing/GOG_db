47bcb6a5bc1345cfe01d0501847b8f6afdc86ea2
alexandre <alexandre.mastaing@gmail.com>
2024-07-23 16:51:58 +0200

    commit_7

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import mysql.connector
+from mysql.connector import errorcode
 
 def get_max_page(url)-> int: 
     """
@@ -35,8 +37,6 @@ def get_link(url:str,max_page:int)->list:
             if link.get('class') == ['product-tile', 'product-tile--grid'] :
                 url_list.append(link.get('href'))
     return url_list
-
-
 
 def get_game_info(url:str,balise_class:str):
     """
@@ -104,3 +104,189 @@ def create_games_dictionary(url_list:list,selector_dict:dict)->dict:
         temp_dict['date'] = get_game_info(url_list, selector_dict['date'])
         games_dict[get_game_info(url_list, selector_dict['title'])] = temp_dict
     return games_dict
+
+def create_database(db_name:str,tables:list,env:list):
+    """Création d'une base de donnée mysql
+    Args:
+        db_name (str): Nom de la base de donnée Mysql à crée
+        tables (list): Listes des differentes tables à crée dans la base de donnée
+        env (dict): liste des information d'environement MySQL sous la forme :  env={host: host_value, user: user_value, password: password_value})
+
+    Returns: Une data-base mysql
+    """
+    mydb = mysql.connector.connect(user=env['user'])
+    cursor = mydb.cursor()
+
+    try:
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name} DEFAULT CHARACTER SET 'utf8'")
+    except mysql.connector.Error as err:
+        print(f"Failed creating database: {db_name}")
+        exit(1)
+
+    try:
+        cursor.execute(f"USE {db_name}")
+        print(f"Database {db_name} exists.")
+    except mysql.connector.Error as err:
+        print(f"Database {db_name} does not exists.")
+        if err.errno == errorcode.ER_BAD_DB_ERROR:
+            create_database(cursor)
+            print(f"Database {db_name} created successfully.")
+            mydb.database = db_name
+        else:
+            print(err)
+            exit(1)
+
+    for table_name in tables:
+        table_description = tables[table_name]
+        try:
+            print(f"Creating table {table_name}: ")
+            cursor.execute(table_description)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("already exists.")
+            else:
+                print(err.msg)
+        else:
+            print("OK")
+
+    cursor.close()
+    mydb.close() 
+
+def insert_db(env:dict,db_name:str,dico:dict):
+    """insertion des données d'un dictionnaire dans une base de donnée MySQL
+
+    Args:
+        env (dict): liste des information d'environement MySQL sous la forme :  env={host: host_value, user: user_value, password: password_value})
+        db_name (str): _description_
+        dico (dict): _description_
+    """
+    # Test de connection 
+    try:
+        mydb = mysql.connector.connect(
+        host=env['host'],
+        user=env['user'],
+        password=env['password'],
+        database=db_name
+        )
+    
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+
+    # Insertion dans la table "games"
+    for game,values in dico.items():
+        name = game
+        base_price = values["base_price"]
+        game_price = values["game_price"]
+        discount = values["reduction"]
+        rating = values["rating"]
+        time_to_beat = values["time_to_beat"]
+        release_date = values["date"]
+        
+        
+        # verification de presence ou non du jeu dans la table "games"
+
+        sql = "SELECT name FROM games WHERE name = (%s) LIMIT 0, 1"
+        val = (name)
+        mycursor.execute(sql, (val,))
+
+        try : 
+            old_name = mycursor.fetchone()[0]
+        except:
+            old_name = ""
+        mycursor = mydb.cursor(buffered=True)
+
+        if name != old_name:
+            sql = "INSERT INTO games (name,base_price,game_price,discount,rating,time_to_beat,release_date) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            val = (name, base_price, game_price, discount, rating, time_to_beat, release_date)
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print(name , " : ajoutée à la table games")
+
+            #liste des langues du jeux 
+            games_languages = values["language"]
+
+            #insertion dans la table "languages"    
+            for language in games_languages:
+                # recupere la langue du jeux dans la database si elle existe déja
+                sql = "SELECT language FROM languages WHERE language = (%s) LIMIT 0, 1"
+                val = (language)
+                mycursor.execute(sql, (val,))
+                try : 
+                    old_language = mycursor.fetchone()[0]
+                except:
+                    old_language = ""
+                mycursor = mydb.cursor(buffered=True)
+
+                if old_language != language:
+                    sql = "INSERT INTO languages (language) VALUES (%s)"
+                    val = (language)
+                    mycursor.execute(sql, (val,))
+                    mydb.commit()
+
+            #liste des  genre du jeux 
+            games_genres = values["genre"]
+
+            
+            #insertion dans la table "genres"
+            for genre in games_genres:
+                # recupere la langue du jeux dans la database si elle existe déja
+                sql = "SELECT genre FROM genres WHERE genre = (%s) LIMIT 0, 1"
+                val = (genre)
+                mycursor.execute(sql, (val,))
+                try : 
+                    old_genre = mycursor.fetchone()[0]
+                except:
+                    old_genre = ""
+                mycursor = mydb.cursor(buffered=True)
+
+                if old_genre != genre:
+                    sql = "INSERT INTO genres (genre) VALUES (%s)"
+                    val = (genre)
+                    mycursor.execute(sql, (val,))
+                    mydb.commit()
+
+            # recupere l'id des jeux
+            sql = "SELECT id FROM games WHERE name = (%s)"
+            val = (name)
+            mycursor.execute(sql, (val,))    
+            id_game = mycursor.fetchone()[0]
+                
+            for language in games_languages:
+                # recupere l'id des langues des jeux
+                sql = "SELECT id FROM languages WHERE language = (%s)"
+                val = (language)
+                mycursor.execute(sql, (val,))
+                language_game = mycursor.fetchone()[0]
+
+                # Insertion dans la table "games_languages"
+                sql = "INSERT INTO games_languages (id_game,language_id) VALUES (%s,%s)"
+                val = (id_game,language_game)
+                mycursor.execute(sql, val)
+                mydb.commit()
+                    
+            for genre in games_genres:
+                # recupere l'id des genres des jeux 
+                sql = "SELECT id FROM genres WHERE genre = (%s)"
+                val = (genre)
+                mycursor.execute(sql, (val,))
+                language_game = mycursor.fetchone()[0]
+
+                # Insertion dans la table "games_genres"
+                sql = "INSERT INTO games_genres (games_id,genre_id) VALUES (%s,%s)"
+                val = (id_game,language_game)
+                mycursor.execute(sql, val)
+                mydb.commit()
+        else:
+            #update des jeux déja présent dans la data base
+            sql = "UPDATE games SET base_price = %s , game_price = %s , discount = %s , rating = %s , time_to_beat = %s , release_date = %s WHERE name = %s"
+            val = (base_price, game_price, discount, rating, time_to_beat, release_date, name)
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print(name , " : actualisé à la table games")
+        
+    mycursor.close()
